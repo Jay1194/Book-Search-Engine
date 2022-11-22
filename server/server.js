053bfc6b276c34357775285d066196ee65f98be9
Jay <j.thomastt1@gmail.com>
2022-11-22 01:54:55 -0600

    finished type defs

--- server/server.js
+++ server/server.js
@@ -5,11 +5,9 @@ const db = require('./config/connection');
 const { ApolloServer } = require('apollo-server-express');
 const { authMiddleware } = require ('./utils/auth');
 const { typeDefs, resolvers } = require('./schemas');
-const { startSession } = require('mongoose');
-const { start } = require('repl');
 
 const app = express();
-const PORT = process.env.PORT || 11000;
+const PORT = process.env.PORT || 3001;
 const server = new ApolloServer({
   typeDefs, resolvers, context: authMiddleware
 });
@@ -17,7 +15,6 @@ const server = new ApolloServer({
 
 server.applyMiddleware({ app });
  
-
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
